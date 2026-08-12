f5b9a7a245cb684b66c0ee8fe805c9b00867ddec
HilmiKamil <hilmihelios@gmail.com>
2026-08-12 20:06:01 +0700

    Create Initial KelanaAI console app

--- backend/task.py
+++ backend/task.py
@@ -1,16 +1,34 @@
+def print_trip_summary(
+    destination,
+    country,
+    days,
+    budget,
+    currency,
+    travel_month
+):
+
+    print("=======================")
+    print("KelanaAI")
+    print("=======================")
+    print(f"Destination     : {destination}")
+    print(f"Country         : {country}")
+    print(f"Days            : {days}")
+    print(f"Budget          : {budget}")
+    print(f"Currency        : {currency}")
+    print(f"Month of Travel : {travel_month}")
+
 destination  = input("Destination : ")
+country      = input("Country : ")
 days         = int(input("days : "))
 budget       = float(input("budget : "))
-travel_style = input("Style : ")
 currency     = input("Currency : ")
 travel_month = input("Month of Travel : ")
 
-print("=======================")
-print("KelanaAI")
-print("=======================")
-print(f"Destination     : {destination}")
-print(f"Days            : {days}")
-print(f"Budget          : {budget}")
-print(f"Style           : {travel_style}")
-print(f"Currency        : {currency}")
-print(f"Month of Travel : {travel_month}")
+print_trip_summary(
+    destination,
+    country,
+    days,
+    budget,
+    currency,
+    travel_month
+)
